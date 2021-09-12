4af94cf54417b7a49b72f44b228ade4f2dc0c2e2
Henrique Montenegro <montenegrohenrique@gmail.com>
2021-09-12 17:31:29 -0300

    01.09 - Problemas na autenticação

--- lib/screens/transaction_form.dart
+++ lib/screens/transaction_form.dart
@@ -68,16 +68,14 @@ class _TransactionFormState extends State<TransactionForm> {
 
                       showDialog(
                           context: context,
-                          builder: (context) {
+                          builder: (contextDialog) {
                             return TransactionAuthDialog(
                               onConfirm: (String password) {
-                                _webClient
-                                    .save(transactionCreated, password)
-                                    .then((transaction) {
-                                  if (transaction != null) {
-                                    Navigator.pop(context);
-                                  }
-                                });
+                                _save(
+                                  transactionCreated,
+                                  password,
+                                  context,
+                                );
                               },
                             );
                           });
@@ -90,5 +88,16 @@ class _TransactionFormState extends State<TransactionForm> {
         ),
       ),
     );
+  }
+
+  void _save(Transaction transactionCreated, String password,
+      BuildContext context) async {
+    // await Future.delayed(Duration(seconds: 1));
+
+    _webClient.save(transactionCreated, password).then((transaction) {
+      if (transaction != null) {
+        Navigator.pop(context);
+      }
+    });
   }
 }
